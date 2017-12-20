d5ca5010ca025d57c0a54f50309cb92adb2316c9
BigZero <duyltk.cs@gmail.com>
2017-12-20 10:05:41 +0700

    Test matrix Half

--- ScoreAA.py
+++ ScoreAA.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import log
 
-def getMatrix(matrixAdj):
+def getMatrixFull(matrixAdj):
     maxNode = len(matrixAdj)
     matrixScore = np.zeros((maxNode * maxNode, 2))
     for i in range(maxNode):
@@ -11,7 +11,6 @@ def getMatrix(matrixAdj):
             tag_class = matrixAdj[i, j]
             matrixScore[rowSRC, 1] = tag_class
             matrixScore[rowTGT, 1] = tag_class
-
             aaScore = 0.0
             for k in range(maxNode):
                 if matrixAdj[i, k] == 1 and matrixAdj[i, k] == matrixAdj[j, k]:
@@ -21,4 +20,20 @@ def getMatrix(matrixAdj):
             matrixScore[rowSRC, 0] = aaScore
             matrixScore[rowTGT, 0] = aaScore
     return matrixScore
+
+def getMatrixHalf(matrixAdj):
+    maxNode = len(matrixAdj)
+    matrixScore = np.empty((0))
+    for i in range(maxNode):
+        for j in range(i + 1, maxNode):
+            aaScore = 0.0
+            for k in range(maxNode):
+                if matrixAdj[i, k] == 1 and matrixAdj[i, k] == matrixAdj[j, k]:
+                    res = np.sum(matrixAdj[k, :])
+                    if log(res) != 0.0:
+                        aaScore += 1.0/log(res)
+                tag_class = matrixAdj[i, j]
+
+
+
             
